0c4be74a532258486456b070f5ce70186e33925b
nishadinfo09 <mdnishadali03@gmail.com>
2026-02-15 20:18:00 +0600

    refactor: comment out response interceptor logic in axios instance

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -5,26 +5,26 @@ const axiosInstance = axios.create({
   withCredentials: true,
 });
 
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
+// axiosInstance.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     const originalRequest = error.config;
 
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
+//     if (error.response?.status === 401 && !originalRequest._retry) {
+//       originalRequest._retry = true;
 
-      try {
-        await axiosInstance.post("/refresh-token");
+//       try {
+//         await axiosInstance.post("/refresh-token");
 
-        return axiosInstance(originalRequest);
-      } catch (refreshError) {
-        // window.location.href = "/login";
-        return Promise.reject(refreshError);
-      }
-    }
+//         return axiosInstance(originalRequest);
+//       } catch (refreshError) {
+//         // window.location.href = "/login";
+//         return Promise.reject(refreshError);
+//       }
+//     }
 
-    return Promise.reject(error);
-  },
-);
+//     return Promise.reject(error);
+//   },
+// );
 
 export default axiosInstance;
